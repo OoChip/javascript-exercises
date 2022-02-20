a859f8f2e1f71e6de1498cacaae0189f569aac2c
OoChip <oochip2001@gmail.com>
2022-02-19 22:17:32 -0500

    resolv 01 to 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+ if (index < 1){return "OOPS" }
+ index = parseInt(index)
+ const fibo = [1,1]
+ for ( let i = 0 ; i < index - 2 ; i++){
+     const newFibo = fibo[i] + fibo[i+1]
+     fibo.push(newFibo)
+ }
+ return fibo.pop()
 };
 
 // Do not edit below this line
